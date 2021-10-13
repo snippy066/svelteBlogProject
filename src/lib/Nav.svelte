da16f8daec213ab76aa4f98b80a33c800b02b498
snippy066 <priyankathakur221206@gmail.com>
2021-10-14 02:03:31 +0530

    Create Nav.svelte

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,25 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/" >HomePage</a>
+        </li>
+        <li>
+            <a href="/about" >About</a>
+        </li>
+        <li>
+            <a href="/contact" >Contact</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    ul{
+        display:flex;
+        list-style: none;
+        margin:0px;
+        padding: 0px;
+    }
+    li{
+        margin-right:20px;
+    }
+</style>
